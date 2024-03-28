f714ed51f9334fe9501456759ca6180d8190a11c
Faruk Furkan Özduran <ffurkan1999@gmail.com>
2024-03-28 03:14:10 +0300

    New challenge
    Inheritance Challenge

---
+++ InheritanceC/InheritanceC/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace InheritanceC
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Faruk Furkan");
+            Console.WriteLine(post1.ToString());
+            
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Faruk Furkan", 
+                "http://images.com/shoes", true);
+            
+            VideoPost videoPost1 = new VideoPost("New Videoo!", "Faruk Furkan", 
+                "http://video.com/video", true, 10);
+
+            videoPost1.Play();
+            Console.WriteLine("Press any key to stop the video!");
+            Console.ReadKey();
+            videoPost1.Stop();
+
+
+            Console.WriteLine(imagePost1.ToString());
+            Console.WriteLine(videoPost1.ToString());
+            Console.ReadLine();
+        }
+
+        // CHALLENGE add a deriving class "VideoPost" with a property VideoURL, Length.
+
+       // create the required constructors to create a VideoPost
+       // adjust the ToString() method accordingly
+       // create an instance of VideoPost
+
+        // More advanced - use Timer and a Callback method here (google it :))
+        // create fields as required
+        // add member methods "Play" which should write the current duration of the video
+        // and "Stop" which should stop the "timer" and write "stopped at {0}s" onto the console.
+        // Play the video after creating the instance and pause it, when the user presses any key.
+    }
+}
---
+++ InheritanceC/InheritanceC/VideoPost.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    internal class VideoPost : Post
+    {
+
+        //member fields
+        protected bool isPlaying = false;
+        protected int currentDuration = 0;
+        Timer timer;
+
+
+        public string VideoURL { get; set; }
+
+        public int Length { get; set; }
+
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, bool isPublic, int length)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            // Property ImageURL is a member of ImagePost, but not of post
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
+
+        public void Play()
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                Console.WriteLine("Playing");
+                timer = new Timer(TimerCallback, null, 0, 1000);
+            }
+            
+        }
+
+        private void TimerCallback(Object o)
+        {
+            if(currentDuration < Length)
+            {
+                currentDuration++;
+                Console.WriteLine("Video at {0}s", currentDuration);
+                GC.Collect();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+        
+        public void Stop() 
+        {
+            if (isPlaying)
+            {
+                isPlaying = false;
+                Console.WriteLine("Stopped at {0}s", currentDuration);
+                currentDuration = 0;
+                timer.Dispose();
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} -- by {3} -- {4}", 
+                this.ID, this.Title, this.Length, this.SendByUsername, this.VideoURL);
+        }
+    }
+}
